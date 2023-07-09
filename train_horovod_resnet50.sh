e3fd8fa0ce97c2ed7618db8577ee8db332c23c28
biyuehuang <31006098+biyuehuang@users.noreply.github.com>
2023-07-09 12:38:29 +0800

    Add files via upload

---
+++ train_horovod_resnet50.sh
@@ -0,0 +1,25 @@
+export OverrideDefaultFP64Settings=1
+export IGC_EnableDPEmulation=1
+NUMBER_OF_PROCESS=2
+PROCESS_PER_NODE=2
+export WORKSPACE=/home/a770/Downloads/kiwi/itex_train/
+MODEL_DIR=${WORKSPACE}/output
+export PYTHONPATH=${WORKSPACE}/tensorflow-models
+DATA_DIR=/home/a770/crystal/arcTrain/datasets/ImageNet100/tf_records/
+
+CONFIG_FILE=${WORKSPACE}/intel-extension-for-tensorflow/examples/train_horovod/resnet50/itex_dummy.yaml
+if [ ! -d "$MODEL_DIR" ]; then
+    mkdir -p $MODEL_DIR
+else
+    rm -rf $MODEL_DIR && mkdir -p $MODEL_DIR
+fi
+
+#horovodrun -np 2 -H localhost:2 \
+python ${PYTHONPATH}/official/vision/image_classification/classifier_trainer.py \
+--mode=train_and_eval \
+--model_type=resnet \
+--dataset=imagenet \
+--model_dir=$MODEL_DIR \
+--data_dir=$DATA_DIR \
+--config_file=$CONFIG_FILE
+
